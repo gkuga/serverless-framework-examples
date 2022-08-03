d3a78902e6e0621615dfac8bdeb50a9d827bb544
gkuga <gaku.yasui@gmail.com>
2022-08-03 16:40:09 +0900

    Add serverless compose sample

---
+++ hello-compose/goodbye-app/handler.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+
+def goodbye(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+        "helloAppAPIURL": os.environ.get('HELLO_API_URL')
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
