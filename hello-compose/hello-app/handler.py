c22828f466567a455adae3dda12c484b22bc7fdd
gkuga <gaku.yasui@gmail.com>
2022-08-18 03:15:18 +0900

    Update

--- hello-compose/hello-app/handler.py
+++ hello-compose/hello-app/handler.py
@@ -10,3 +10,14 @@ def hello(event, context):
     response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
+
+
+def auth(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
