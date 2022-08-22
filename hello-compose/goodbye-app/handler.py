1f8796e0af0a3b921033dd2430b89cee3b0dfa12
gkuga <gaku.yasui@gmail.com>
2022-08-23 00:44:48 +0900

    update

--- hello-compose/goodbye-app/handler.py
+++ hello-compose/goodbye-app/handler.py
@@ -6,7 +6,8 @@ def goodbye(event, context):
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event,
-        "helloAppAPIURL": os.environ.get('HELLO_API_URL')
+        "helloAppAPIURL": os.environ.get('HELLO_API_URL'),
+        "envTest": os.environ.get('ENV_TEST'),
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
